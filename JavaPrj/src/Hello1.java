7f2b72c13770a23c7ac45e13166d01840fbdea5e
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-08 11:33:32 +0900

    두번째 올립니다.

---
+++ JavaPrj/src/Hello1.java
@@ -0,0 +1,14 @@
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=5;
+		int c=3;
+		
+		a=b+c;
+		System.out.printf("%d + %d = %d \n",b,c,a);
+	}
+
+}
