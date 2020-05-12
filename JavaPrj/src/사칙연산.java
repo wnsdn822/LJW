3ab409dd6372f73d129cd3b0828d51669d20139e
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-12 10:41:40 +0900

    사칙연산

---
+++ JavaPrj/src/사칙연산.java
@@ -0,0 +1,25 @@
+
+public class 사칙연산 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		int result;
+		
+		a=100;
+		b=50;
+		
+		result = a+b;
+		System.out.println(a + "+" + b + "=" + result);
+		
+		result = a-b;
+		System.out.println(a+"-"+b+"="+result);
+		
+		result = a*b;
+		System.out.println(a + "*" + b + "=" + result);
+		
+		result = a/b;
+		System.out.println(a + "/" + b + "=" + result);
+	}
+
+}
