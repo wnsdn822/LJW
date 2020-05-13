9ff862770571cb95a9f970d0bd7b0e9efc278dc7
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-13 17:00:57 +0900

    HI~

---
+++ JavaPrj/src/Hello5.java
@@ -0,0 +1,18 @@
+
+public class Hello5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("%d\n",123);
+		System.out.printf("%5d\n",123);
+		System.out.printf("%05d\n",123);
+		
+		System.out.printf("%f\n",123.45);
+		System.out.printf("%7.1f\n",123.45);
+		System.out.printf("%7.3f\n",123.45);
+		
+		System.out.printf("%s\n","Hi~");
+		System.out.printf("%10s\n","HI~");
+	}
+
+}
