6b2395a335e190545322eb9bec3b0e6db081506c
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-13 17:24:50 +0900

    HI~

---
+++ JavaPrj/src/Hello7.java
@@ -0,0 +1,16 @@
+
+public class Hello7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap=0;
+		int i;
+		
+		for(i=1; i<=10; i++) {
+			hap +=i;
+		}
+		
+		System.out.printf("1에서 10까지의 합: %d \n",hap);
+	}
+
+}
