39891ca0b68389d53f09dfd1539a540494f60063
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-15 10:18:31 +0900

    while~

---
+++ JavaPrj/src/Hello00.java
@@ -0,0 +1,17 @@
+
+public class Hello00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int hap=0;
+		int i;
+		
+		i=1;
+		while(i<=10) {
+			hap+=i;
+			i++;
+		}
+		System.out.printf("1에서 10까지의합: %d\n", hap);
+	}
+
+}
