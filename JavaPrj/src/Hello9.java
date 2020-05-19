13f025f0eaa7eb56d642ffc9ab62277439b48666
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-19 09:48:10 +0900

    scanner

---
+++ JavaPrj/src/Hello9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hello9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		
+		int n;
+		n=s.nextInt();
+		System.out.printf("%d",n);
+	}
+
+}
