274b1a8121f5fb93cacfe671b8a46424be305777
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-19 12:22:36 +0900

    HI~

---
+++ JavaPrj/src/Char.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Char {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+	Scanner s =new Scanner(System.in);
+	char x;
+	
+	x=(char)System.in.read();
+	System.out.println(x);
+	
+	}
+
+}
