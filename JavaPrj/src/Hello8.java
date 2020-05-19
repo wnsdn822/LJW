4747b95a2ee212812ff0223783afde1b440433ed
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-19 09:40:34 +0900

    HI~

---
+++ JavaPrj/src/Hello8.java
@@ -0,0 +1,11 @@
+
+public class Hello8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("\u250C\u252C\u2510");
+	     System.out.println("\u251C\u253c\u2524");
+	     System.out.println("\u2514\u2534\u2518");
+	}
+
+}
