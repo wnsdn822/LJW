054de32d43e180e74fcb9765d5f3c436c73b7d6a
DATA8320-20 <DATA8320-20@DESKTOP-TNN6DSU>
2020-05-22 09:58:07 +0900

    GU~

---
+++ JavaPrj/src/Break.java
@@ -0,0 +1,17 @@
+
+public class Break {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		
+		for(i=1; i<=100; i++) {
+			
+			System.out.printf("for뮨울 %d회 실행했습니다 \n", i);
+			if(i==57)
+			break;
+		}
+		System.out.printf("for문을 종료했습니다. \n");
+	}
+
+}
