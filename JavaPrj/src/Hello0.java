1b58b6e4c13b379c9d34e24a73ef398753211e8d
DATA8320-20 <wnsdn822@naver.com>
2020-06-01 16:38:44 +0900

    HI~

---
+++ JavaPrj/src/Hello0.java
@@ -0,0 +1,27 @@
+
+public class Hello0 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=2;
+		int j;
+		int c=0;
+		
+		while(i<=9) {
+			if(i==3) {
+				System.out.println("3´Ü");}
+			if(i==5){
+				System.out.println("5´Ü");}
+			}
+			j=1;
+			while(j<=9) {
+				c=i*j;
+				System.out.println(i+"*"+j+"="+c);
+				j++;
+				
+			}
+			i++;
+			System.out.println();
+		}
+	}
+
