e914a59011e735d83371b73ca3d83a67db1c84dc
DATA8320-20 <wnsdn822@naver.com>
2020-06-03 08:52:58 +0900

    HI~

---
+++ JavaPrj/src/Cat.java
@@ -0,0 +1,12 @@
+
+public class Cat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+
+}
