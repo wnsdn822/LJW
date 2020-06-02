1594251a47f6f5dec7d00ed8b482981b0603a98d
DATA8320-20 <wnsdn822@naver.com>
2020-06-02 10:52:00 +0900

    HI~

---
+++ JavaPrj/src/BaekJoon_1001.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class BaekJoon_1001{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a= s.nextInt();
+        int b= s.nextInt();
+        System.out.println(a-b);
+    }
+}
