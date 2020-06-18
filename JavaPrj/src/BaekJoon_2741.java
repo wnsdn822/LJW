0d6aacf4d3ee3b7a46d11ffac4a5935fdd6e7645
DATA8320-20 <wnsdn822@naver.com>
2020-06-18 09:50:38 +0900

    HI~

---
+++ JavaPrj/src/BaekJoon_2741.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class BaekJoon_2741 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int a;
+		int b;
+		b = s.nextInt();
+		for(a=1; a<=b; a++) {
+			System.out.println(a);
+		}
+		
+	}
+
+}
